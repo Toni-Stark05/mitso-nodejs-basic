f7ee500b0e1f87d557edd1f717028f4325649ee6
tonistark <nikidysbdbdy@gmail.com>
2025-02-03 09:23:01 +0300

    feat: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const filePath = 'files/wrongFilename.txt'
+    const newFilePath = 'files/properFilename.md'
+
+    if (fs.existsSync(newFilePath)) {
+        throw new Error(`FS operation failed`);
+    }
+
+    try {
+        await fs.renameSync(filePath, newFilePath);
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await rename();
