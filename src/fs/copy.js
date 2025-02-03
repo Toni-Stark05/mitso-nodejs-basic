f4b98bb582a5be5505d1161cc13bb63cf14ed912
tonistark <nikidysbdbdy@gmail.com>
2025-02-03 09:16:16 +0300

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    const drtPath = './files'
+    const copyDrtPath = './files_copy'
+
+    if (fs.existsSync(copyDrtPath)) {
+        throw new Error(`FS operation failed`);
+    }
+
+    try {
+        await fs.mkdirSync(copyDrtPath);
+
+        const files = fs.readdirSync(drtPath);
+        for (const file of files) {
+            fs.copyFileSync(`${drtPath}/${file}`, `${copyDrtPath}/${file}`);
+        }
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await copy();
