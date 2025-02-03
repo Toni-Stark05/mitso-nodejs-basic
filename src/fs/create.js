5a2df7463f54596c9d0e9a47ad2b920ec78789aa
tonistark <nikidysbdbdy@gmail.com>
2025-02-03 08:53:31 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    const context = 'I am fresh and young'
+    const filePath = 'files/fresh.txt'
+
+    try {
+        if (fs.existsSync(filePath)) {
+            throw new Error(`The file exists, path: ${filePath}`);
+        }
+        await fs.writeFileSync(filePath, context)
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await create();
